b0b1595fc1e422cea816d88ea1f5df61973ffd1e
monotomy <info@monoplugs.com>
2015-08-19 09:03:42 +0200

    remove unneeded midi options

--- Source/UiEditorMIDIIO.h
+++ Source/UiEditorMIDIIO.h
@@ -7,12 +7,12 @@
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Created with Introjucer version: 3.1.1
+  Created with Introjucer version: 3.2.0
 
   ------------------------------------------------------------------------------
 
   The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
-  Copyright 2004-13 by Raw Material Software Ltd.
+  Copyright (c) 2015 - ROLI Ltd.
 
   ==============================================================================
 */
@@ -86,57 +86,13 @@ private:
     ScopedPointer<Label> label_9;
     ScopedPointer<ComboBox> combo_output_cc;
     ScopedPointer<ToggleButton> toggle_output_cc_mute;
-    ScopedPointer<ToggleButton> toggle_output_thru_mute;
-    ScopedPointer<Label> label_11;
-    ScopedPointer<ComboBox> combo_output_lfo_1;
-    ScopedPointer<ComboBox> combo_output_lfo_channel_1;
-    ScopedPointer<Label> label_12;
-    ScopedPointer<ToggleButton> toggle_output_lfo_mute_1;
-    ScopedPointer<ComboBox> combo_output_lfo_number_1;
-    ScopedPointer<Label> label_15;
-    ScopedPointer<ComboBox> combo_output_lfo_2;
-    ScopedPointer<ComboBox> combo_output_lfo_channel_2;
-    ScopedPointer<ToggleButton> toggle_output_lfo_mute_2;
-    ScopedPointer<ComboBox> combo_output_lfo_number_2;
-    ScopedPointer<Label> label_16;
-    ScopedPointer<ComboBox> combo_output_lfo_3;
-    ScopedPointer<ComboBox> combo_output_lfo_channel_3;
-    ScopedPointer<ToggleButton> toggle_output_lfo_mute_3;
-    ScopedPointer<ComboBox> combo_output_lfo_number_3;
-    ScopedPointer<ToggleButton> toggle_output_lfo_mute_5;
-    ScopedPointer<Label> label_19;
-    ScopedPointer<ComboBox> combo_output_adsr_1;
-    ScopedPointer<ComboBox> combo_output_adsr_channel_1;
-    ScopedPointer<ToggleButton> toggle_output_adsr_mute_1;
-    ScopedPointer<ComboBox> combo_output_adsr_number_1;
-    ScopedPointer<Label> label_20;
-    ScopedPointer<ComboBox> combo_output_adsr_2;
-    ScopedPointer<ComboBox> combo_output_adsr_channel_2;
-    ScopedPointer<ToggleButton> toggle_output_adsr_mute_2;
-    ScopedPointer<ComboBox> combo_output_adsr_number_2;
-    ScopedPointer<Label> label_21;
-    ScopedPointer<ComboBox> combo_output_adsr_3;
-    ScopedPointer<ComboBox> combo_output_adsr_channel_3;
-    ScopedPointer<ToggleButton> toggle_output_adsr_mute_3;
-    ScopedPointer<ComboBox> combo_output_adsr_number_3;
-    ScopedPointer<Label> label_22;
-    ScopedPointer<ComboBox> combo_output_adsr_4;
-    ScopedPointer<ComboBox> combo_output_adsr_channel_4;
-    ScopedPointer<ToggleButton> toggle_output_adsr_mute_4;
-    ScopedPointer<ComboBox> combo_output_adsr_number_4;
     ScopedPointer<Label> label_23;
     ScopedPointer<ComboBox> combo_output_clock;
-    ScopedPointer<ToggleButton> toggle_output_clock_mute;
-    ScopedPointer<Label> label_32;
-    ScopedPointer<Label> label_33;
-    ScopedPointer<Label> label_34;
-    ScopedPointer<Label> label_35;
-    ScopedPointer<Label> label_13;
     ScopedPointer<TextButton> close;
-    ScopedPointer<Label> label_38;
     ScopedPointer<Slider> slider_midi_pickup;
     ScopedPointer<TextButton> button_midi_learn;
     ScopedPointer<Label> label_17;
+    ScopedPointer<Label> label_2;
 
 
     //==============================================================================
