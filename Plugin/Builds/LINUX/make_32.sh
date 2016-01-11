f89a2fe89a265b3814b280b514aa0d9ed39a358f
monotomy <monotomy@localhost.localdomain>
2016-01-11 15:25:07 +0100

    BACKUP

--- Plugin/Builds/LINUX/make_32.sh
+++ Plugin/Builds/LINUX/make_32.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-konsole -e /bin/sh -c "cd $(dirname "$0") && echo 'BUILD LINUX MONIQUE 32 PLUGIN' && make clean CONFIG=x64 && make CONFIG=x32 -j6 && notify-send 'BUILD LINUX MONIQUE 32 PLUGIN - DONE'" 
+konsole -e /bin/sh -c "cd $(dirname "$0") && echo 'BUILD LINUX MONIQUE 32 PLUGIN' && make clean CONFIG=x32 && make CONFIG=x32 -j3 && make strip CONFIG=x32 && notify-send 'BUILD LINUX MONIQUE 32 PLUGIN - DONE'" 
