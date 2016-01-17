1b6be748f9f23989dba4172ae304b0f693daf371
monotomy <info@monoplugs.com>
2016-01-17 09:31:01 +0100

    add old segement meter code

--- Source/monique_ui_SegmentedMeter.cpp
+++ Source/monique_ui_SegmentedMeter.cpp
@@ -145,3 +145,159 @@ void Monique_Ui_SegmentedMeter::paint (Graphics &g)
                      false);
     }
 }
+
+/*
+#include "monique_ui_SegmentedMeter.h"
+#include "monique_ui_LookAndFeel.h"
+
+#define NUM_RED_SEG 2
+#define NUM_YELLOW_SEG 3
+#define NUM_GREEN_SEG 4
+#define TOTAL_NUM_SEG (NUM_RED_SEG+NUM_YELLOW_SEG+NUM_GREEN_SEG)
+
+#define DB_PER_SEC 3.0f
+#define SAMPLES_TO_COUNT 2048
+
+COLD Monique_Ui_SegmentedMeter::Monique_Ui_SegmentedMeter( Monique_Ui_Refresher*const ui_refresher_ ) noexcept
+:
+Monique_Ui_Refreshable(ui_refresher_),
+                       sampleMax     (0.0f),
+                       level         (0.0f),
+
+                       numSegs       (0),
+                       last_numSeg   (-1),
+                       sampleCount   (0),
+
+                       my_yellow_ref( look_and_feel->colours.get_theme( COLOUR_THEMES::MASTER_THEME ).button_on_colour ),
+                       my_green_ref( look_and_feel->colours.get_theme( COLOUR_THEMES::MASTER_THEME ).button_off_colour ),
+                       my_bg_ref( look_and_feel->colours.get_theme( COLOUR_THEMES::MASTER_THEME ).area_colour ),
+
+                       my_red( Colours::red ),
+                       my_yellow( my_yellow_ref ),
+                       my_green( my_green_ref ),
+                       my_bg( my_bg_ref ),
+
+                       needsRepaint  (true)
+{
+    setOpaque (true);
+}
+
+COLD Monique_Ui_SegmentedMeter::~Monique_Ui_SegmentedMeter() noexcept {}
+
+void Monique_Ui_SegmentedMeter::refresh() noexcept
+{
+    // map decibels to numSegs
+    numSegs = jmax (0, roundToInt ((toDecibels_fast(level) / DB_PER_SEC) + (TOTAL_NUM_SEG - NUM_RED_SEG)));
+    level *= 0.8f;
+    if( numSegs != last_numSeg || needsRepaint)
+    {
+        last_numSeg = numSegs;
+        repaint();
+    }
+}
+
+void Monique_Ui_SegmentedMeter::resized()
+{
+    const float w = getWidth();
+    const float h = getHeight();
+
+    onImage = Image (Image::ARGB,
+                     w, h,
+                     true);
+    offImage = Image (Image::ARGB,
+                      w, h,
+                      true);
+
+    Graphics gOn (onImage);
+    Graphics gOff (offImage);
+
+    const float segmentWidth = (w-1) / TOTAL_NUM_SEG;
+    for (int i = 1; i <= TOTAL_NUM_SEG; ++i)
+    {
+        Colour colour_on;
+        Colour colour_off;
+
+        if (i <= NUM_GREEN_SEG)
+        {
+            colour_on = my_green_ref.brighter(0.25);
+            colour_off = my_green_ref.darker(1).darker(0.5);
+        }
+        else if (i <= (NUM_YELLOW_SEG + NUM_GREEN_SEG))
+        {
+            colour_on = my_yellow_ref.brighter(0.25);
+            colour_off = my_yellow_ref.darker(1).darker(0.5);
+        }
+        else
+        {
+            colour_on = my_red.brighter(0.25);
+            colour_off = my_red.darker(1).darker(0.5);
+        }
+
+        const float x = w - (segmentWidth*i);
+
+        gOn.setColour (colour_on);
+        gOn.fillRoundedRectangle (x,1.0f, segmentWidth-2, h-2, 0);
+
+        gOff.setColour (colour_off.darker(0.3));
+        gOff.fillRoundedRectangle (x,1.0f, segmentWidth-2, h-2, 0);
+        gOff.setColour (colour_off.darker (0.6f));
+        gOff.drawRoundedRectangle (x,1.0f, segmentWidth-2, h-2, 0, 1);
+
+    }
+
+    needsRepaint = true;
+}
+
+void Monique_Ui_SegmentedMeter::moved()
+{
+    needsRepaint = true;
+}
+
+void Monique_Ui_SegmentedMeter::paint (Graphics &g)
+{
+    if( my_yellow != my_yellow_ref or my_green != my_green_ref or my_bg != my_bg_ref )
+    {
+        my_yellow = my_yellow_ref;
+        my_green = my_green_ref;
+        my_bg = my_bg_ref;
+	
+        offImage.clear(offImage.getBounds());
+        onImage.clear(onImage.getBounds());
+
+	needsRepaint = true;
+	
+        resized();
+        refresh();
+        return;
+    }
+
+    needsRepaint = false;
+
+    const int w = getWidth();
+    const int h = getHeight();
+
+    g.fillAll(Colour(0xff050505));
+
+    g.addTransform( AffineTransform::rotation
+                    (
+                        (float) (180.0f / (180.0 / double_Pi)),
+                        0.5f*w,
+                        0.5f*h
+                    )
+                  );
+
+    if (onImage.isValid())
+    {
+        g.drawImage (onImage,
+                     0, 0, w, h,
+                     0, 0, w, h,
+                     false);
+
+        const int offWidth = w - jmin(w,jmax(0,roundToInt((float(numSegs) / TOTAL_NUM_SEG) * onImage.getWidth())));
+        g.drawImage (offImage,
+                     0, 0, offWidth, h,
+                     0, 0, offWidth, h,
+                     false);
+    }
+}
+*/
--- Source/monique_ui_SegmentedMeter.h
+++ Source/monique_ui_SegmentedMeter.h
@@ -59,3 +59,69 @@ inline void Monique_Ui_SegmentedMeter::process( const float* values, int num_sam
 }
 
 #endif //__DROWAUDIO_Monique_Ui_SegmentedMeter_H__
+
+/*
+#ifndef __Monique_Ui_SegmentedMeter_H__
+#define __Monique_Ui_SegmentedMeter_H__
+
+#include "App_h_includer.h"
+
+// SIMPLYFYED DROW AUDIO SEGMENT METER
+class Monique_Ui_SegmentedMeter
+    :
+public Component,
+public Monique_Ui_Refreshable
+{
+    Image onImage, offImage;
+
+    float sampleMax;
+    float level;
+
+    int numSegs, last_numSeg;
+    int sampleCount;
+
+    const Colour& my_yellow_ref, my_green_ref, my_bg_ref;
+    Colour my_red, my_yellow, my_green, my_bg;
+
+    bool needsRepaint;
+
+public:
+    COLD Monique_Ui_SegmentedMeter( Monique_Ui_Refresher*const ui_refresher_ ) noexcept;
+    COLD ~Monique_Ui_SegmentedMeter() noexcept;
+
+private:
+    void resized() override;
+    void paint (Graphics &g) override;
+    void moved() override;
+    void refresh() noexcept override;
+
+public:
+    inline void process( const float* values, int numSamples ) noexcept;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Monique_Ui_SegmentedMeter);
+};
+
+inline void Monique_Ui_SegmentedMeter::process( const float* values, int num_samples_ ) noexcept
+{
+    for (int sid = 0; sid < num_samples_; ++sid)
+    {
+        using namespace std;
+        float sample = fabsf (values[sid]);
+
+        if (sample > sampleMax)
+            sampleMax = sample;
+
+        if (++sampleCount == 2048) // 1024 == release/decay
+        {
+            if (sampleMax > level)
+                level = sampleMax;
+
+            sampleMax = 0;
+            sampleCount = 0;
+        }
+    }
+}
+
+#endif //__DROWAUDIO_Monique_Ui_SegmentedMeter_H__
+
+*/
